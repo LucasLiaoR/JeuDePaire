ed60bb928b7c07051989c9e59c8e616f5089c2f5
LucasLiaoR <lucasliao6@gmail.com>
2021-03-17 10:52:47 +0100

    Upload du code source

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Paires";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Générer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.Location = new System.Drawing.Point(20, 97);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(764, 334);
+            this.panel1.TabIndex = 3;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel1_DragDrop);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.button_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Jeu de paire";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void creer_button(int x, int y, String number)
+        {
+            Label btn = new Label();
+
+            panel1.Controls.Add(btn);
+
+            btn.Left = x;
+            btn.Top = y;
+            btn.Text = number;
+            btn.Size = new Size(34, 34);
+            btn.AllowDrop = true;
+            btn.BackColor = Color.White;
+            btn.BorderStyle = BorderStyle.FixedSingle;
+            btn.TextAlign = ContentAlignment.MiddleCenter;
+
+            btn.MouseDown += new MouseEventHandler(button_MouseDown);
+            btn.DragEnter += new DragEventHandler(button_DragEnter);
+            btn.DragDrop += new DragEventHandler(button_DragDrop);
+
+        }
+
+        String value_drag;
+        int index_btn_depart;
+
+
+        public void button_MouseDown(object sender, MouseEventArgs e)
+        {
+            Label butn = (Label)sender;
+
+            value_drag = butn.Text;
+            index_btn_depart = panel1.Controls.IndexOf(butn);
+            butn.DoDragDrop("bidon", DragDropEffects.All);
+        }
+
+        public void button_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        public void button_DragDrop(object sender, DragEventArgs e)
+        {
+            Label btn = (Label)sender;
+
+            if (value_drag == btn.Text && index_btn_depart != panel1.Controls.IndexOf(btn))
+            {
+                panel1.Controls.RemoveAt(index_btn_depart);
+                panel1.Controls.Remove(btn);
+                MessageBox.Show("Bien joué!", "Trouvé!", MessageBoxButtons.OK);
+                if (panel1.Controls.Count == 0)
+                {
+                    MessageBox.Show("Félicitations!", "Gagné!", MessageBoxButtons.OK);
+                    button1.Enabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Raté!", "Pas la bonne paire!", MessageBoxButtons.OK);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rand = new Random();
+
+            int x = 0;
+            int y = 0;
+            String number ="";
+
+            if (textBox1.Text != "")
+            {
+                button1.Enabled = false;
+                for (int i = 1; i <= Int16.Parse(textBox1.Text); i++)
+                {
+                    number = i.ToString();
+                    x = rand.Next(700);
+                    y = rand.Next(300);
+                    creer_button(x,y, number);
+                    x = rand.Next(700);
+                    y = rand.Next(300);
+                    creer_button(x, y, number);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vous n'avez pas saisi de nombre de paire !", "ERREUR !", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void panel1_DragDrop(object sender, DragEventArgs e)
+        {
+            panel1.Controls[index_btn_depart].Top = e.Y - this.Top - 150;
+            panel1.Controls[index_btn_depart].Left = e.X - this.Left - 50;
+        }
+    }
+}
